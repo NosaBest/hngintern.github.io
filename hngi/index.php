f16730c1aac2fa201e0fcbbd81771d58af570f5c
Orobosa Nosakhare <orobosa.nosakhare218@outlook.com>
2019-09-21 18:13:43 +0100

    Still working
    Working on a login page.

---
+++ hngi/index.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+
+    <!-- fontawesome cdn -->
+	<script src="https://use.fontawesome.com/e95a82a84b.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<script>
+	window.alert(5 + 6);
+	</script>
+
+
+    <title>HNG INTERN</title>
+  </head>
+  <body class="bg-light">
+   <!-- navbar -->
+   <nav class="navbar navbar-expand-md navbar-light sticky-top">
+   <div class="container-fluid">
+   	<a class="navbar-brand" href="#"><img src="images/logo1.png" height="55px" width="100px"></a>
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+		<span class="navbar-toggler-icon"></span>
+	</button>	
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="#">Home</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">About us</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Blog</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Forum</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="#">Team</a>
+			</li>
+		</ul>
+	</div>
+   </div>
+   </nav>
+
+   <!-- login -->
+  
+   	<div class="container bg-white" >
+   		<div class="row text-center justify-content-center align-items-center">
+   			<div class="col-12 mt-5">
+   				<h1 class="display-5 lead"> Welcome! <i class="fa fa-smile-o display-2 text-success" aria-hidden="true" ></i>
+</h1>
+   			</div>
+
+   		<div class="col-xl-3 col-md-3 col-sm-12 col-xs-12">
+   			<form action="#">
+				<div class="form-group">
+				  <input type="text" class="form-control" placeholder="Username">
+				 </div>
+
+				    <div class="form-group">
+				      <input type="Password" class="form-control" placeholder="Password">
+
+				      <small class="post"><a href="#">forgot password?</a></small>
+				    </div>
+
+				    <div class="form-group">
+			  		<button type="submit" class="btn btn-outline-primary" onclick="test()">Login</button>
+			  		</div>
+			  		<div class="mb-3">
+			  			<small>or</small>
+			  		</div>
+
+			  		<div class="form-group">
+			  		<button type="submit" class="btn btn-outline-danger "><i class="fa fa-google" aria-hidden="true"></i>
+ 					Sign in with google</button>
+			  		</div>
+
+			  		<div>
+			  			<small class="">Not a member? <a href="#">Sign Up</a> now.</small>
+			  		</div>
+			</form>
+
+					<div class="mt-5 mb-5">
+			  			<small class="text-muted">powered by Nosa webdev</small>
+			  		</div>
+
+		</div>
+
+		</div>
+	</div>
+
+
+
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
